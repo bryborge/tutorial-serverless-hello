b7a4688f25654d3b4fcd49386afae911d6339ca1
sonofborge <sonofborge@protonmail.com>
2019-12-11 23:02:00 -0800

    pass name through url params to get a personalized response

--- handler.js
+++ handler.js
@@ -1,12 +1,19 @@
 'use strict';
 
 module.exports.hello = async event => {
+  let name = 'World';
+
+  // If there are query params and a query param is called 'name', assign it to the name variable.
+  if (event.queryStringParameters && event.queryStringParameters.name) {
+    name = event.queryStringParameters.name;
+  }
+
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Hello, World!',
-        input: event,
+        message: `Hello, ${name}!`,
+        event
       },
       null,
       2
